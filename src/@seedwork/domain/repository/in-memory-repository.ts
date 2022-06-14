7c60dc15ba0cbb087db65d57eccb822c84503e1d
Andrade, Sérgio <sergiovenicio2015@gmail.com>
2022-06-14 08:32:47 -0300

    feat: searchable repository

--- src/@seedwork/domain/repository/in-memory-repository.ts
+++ src/@seedwork/domain/repository/in-memory-repository.ts
@@ -1,7 +1,7 @@
 import Entity from '../entity/entity';
 import NotFoundError from '../errors/not-found.error';
 import UniqueEntityId from '../value-objects/unique-entity-id.vo';
-import RepositoryInterface from './repository-interface';
+import RepositoryInterface, { SearchableRepisitoryInterface } from './repository-interface';
 
 abstract class InMemoryRepository<T extends Entity> implements RepositoryInterface<T> {
   private items: T[] = []
@@ -19,13 +19,14 @@ abstract class InMemoryRepository<T extends Entity> implements RepositoryInterfa
   }
 
   async update(entity: T): Promise<void> {
+    await this._get(`${entity.id}`)
     const indexFound = this.items.findIndex(item => item.id === entity.id)
     this.items[indexFound] = entity
   }
 
   async delete(id: string | UniqueEntityId): Promise<void> {
     const item = await this._get(`${id}`)
-    const indexFound = this.items.findIndex(i => i.id === item.id)
+    const indexFound = this.items.findIndex(i => `${i.id}` === `${item.id}`)
     this.items.splice(indexFound, 1)
   }
 
@@ -42,4 +43,18 @@ abstract class InMemoryRepository<T extends Entity> implements RepositoryInterfa
   }
 }
 
-export default InMemoryRepository
+
+class SearchParams {}
+
+abstract class InMemorySearchableRepository<
+  E extends Entity,
+  SearchParams,
+  SearchResult
+> extends InMemoryRepository<E>
+  implements SearchableRepisitoryInterface<E, SearchParams, SearchResult> {
+    search(props: SearchParams): Promise<SearchResult> {
+      throw new Error('Method not implemented.');
+    }  
+  }
+
+export { InMemoryRepository, InMemorySearchableRepository }
--- src/@seedwork/domain/repository/repository-interface.ts
+++ src/@seedwork/domain/repository/repository-interface.ts
@@ -9,4 +9,110 @@ interface RepositoryInterface<T extends Entity> {
   delete(id: string | UniqueEntityId): Promise<void>
 }
 
+type SortDirection = 'asc' | 'desc'
+
+type SearchProps<Filter = string> = {
+  page?: number,
+  per_page?: number,
+  sort?: string,
+  sort_dir?: SortDirection,
+  filter?: Filter
+}
+
+class SearchParams {
+  protected _page: number = 1
+  protected _per_page: number = 15
+  protected _sort: string | null = null
+  protected _sort_dir: SortDirection | null = null
+  protected _filter: string | null = null
+
+  constructor(props: SearchProps) {
+    this.page = props.page ?? 1
+    this.per_page = props.per_page ?? 50
+    this.sort = props.sort ?? null
+    this.sort_dir = props.sort_dir ?? 'asc'
+  }
+
+  get page(): number {
+    return this._page
+  }
+
+  private set page(value: number) {
+    let page = +value
+
+    if(Number.isNaN(page) || page <= 0) {
+      page = 1
+    }
+
+    this._page = parseInt(page as any)
+  }
+
+  get per_page(): number {
+    return Number(this._per_page)
+  }
+
+  set per_page(value: number) {
+    let per_page = +value
+
+    if(Number.isNaN(per_page) || per_page <= 0) {
+      per_page = 1
+    }
+
+    this._per_page = parseInt(per_page as any)
+  }
+
+  get sort(): string | null {
+    return this._sort
+  }
+
+  private set sort(value: string | null) {
+    this._sort = (
+      value === null || value === undefined || value === ""
+    ) ? null : `${value}`
+  }
+
+  get sort_dir(): SortDirection | null {
+    return this._sort_dir
+  }
+
+  private set sort_dir(value: SortDirection | null) {
+    if(!this.sort) {
+      this.sort_dir = null
+      return
+    }
+
+    const dir = String(value).toLowerCase()
+    this._sort_dir = dir !==  'asc' &&  dir !== 'desc' ? 'asc': dir
+  }
+
+  get filter(): string | null {
+    return this._filter
+  }
+
+  private set filter(value: string | null) {
+    this._filter = (
+      value === null || value === undefined || value === ""
+    ) ? null : `${value}`
+  }
+}
+
+class SearchResult {
+
+}
+
+interface SearchableRepisitoryInterface<
+  E extends Entity,
+  SearchInput,
+  SearchOutput
+> extends RepositoryInterface<E> {
+  search(props: SearchInput): Promise<SearchOutput>
+}
+
+export {
+  SearchableRepisitoryInterface,
+  SearchParams,
+  SearchResult,
+  SearchProps,
+  SortDirection
+}
 export default RepositoryInterface
